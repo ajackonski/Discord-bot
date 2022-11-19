d3401e7151deb0549148769e0b5792299461f74d
Alex Graff <infostatus200@gmail.com>
2022-11-18 21:28:11 -0800

    testing different user

--- client.py
+++ client.py
@@ -4,6 +4,6 @@ class MyClient(discord.Client):
         print('Logged on as', self.user)
 
     async def on_message(self, message):
-        if message.author.name == "Rock Muncha" and message.author.discriminator == "7509":
+        if message.author.name == "AlexJ":
             if len(message.content) > 240:
                 await message.channel.send('Dante please be quiet you\'re scaring the hoes')
